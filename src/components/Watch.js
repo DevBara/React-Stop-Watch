c8989f80ac905a3ac1c19f93ddc79eba4da9eba0
DevBara <61251493+DevBara@users.noreply.github.com>
2020-03-28 21:21:21 -0500

    if statement, start, reset

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -10,6 +10,19 @@ export default class Watch extends Component {
         this.state ={
             time:0
         };
+
+        timer(event){
+            //create variable event when buttons are clicked
+            const buttonClicked = event.target.id
+            //if statement that stats what each button does
+            //to track time, use JS method setInterval()
+            if(buttonClicked === 'start'){
+                let id = setInterval(this.startTimer ,1000) //1000 is in milliseconds = 1 second
+                this.setState({clearId:id}); //To update State properly, use setState()
+            } else if(buttonClicked === 'reset'){
+                this.setState(time:0) //once reset button is clicked, reset time to 0
+            }
+        }
     }
     render() {
         return (
