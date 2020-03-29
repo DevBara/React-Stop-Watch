1127cd98722bc00e20360e979c1c03890959ad1c
DevBara <61251493+DevBara@users.noreply.github.com>
2020-03-28 20:36:54 -0500

    created constructor

---
+++ src/components/Watch.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+
+
+
+
+
+export default class Watch extends Component {
+    constructor(props){
+        super(props);
+
+        this.state ={
+            time:0
+        };
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
