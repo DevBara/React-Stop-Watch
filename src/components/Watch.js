4c80a89df0adc0a42daadf91ff7e1ded6f54236a
DevBara <61251493+DevBara@users.noreply.github.com>
2020-03-28 21:13:18 -0500

    created timer function to track time

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react'
 
 
 
-
-
 export default class Watch extends Component {
     constructor(props){
         super(props);
@@ -16,7 +14,8 @@ export default class Watch extends Component {
     render() {
         return (
             <div>
-                
+            {/* Header 1 should be the time that is called from state */}
+               <h1>{this.state.time}</h1> 
             </div>
         )
     }
